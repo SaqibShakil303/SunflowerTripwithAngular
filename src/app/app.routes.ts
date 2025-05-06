db4bb5e520dd3727ecfd88fedbcbcec9add41eb0
SaqibShakil303 <mds168723@gmail.com>
2025-05-06 17:10:06 +0530

    added poppins and fixed routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,7 +15,7 @@ export const routes: Routes = [
     // { path: 'home', component: HomeComponent },
     // { path: '', redirectTo: 'home', pathMatch: 'full' },
     // { path: '**', redirectTo: '' },
-    {path: 'home',
+    {path: '',
     component: HomeComponent,
     children: [
       { path: 'about', component: AboutComponent },
