e2d71c5c43570e941b34b9b2216ab30236706d98
SaqibShakil303 <mds168723@gmail.com>
2025-05-21 10:35:45 +0530

    removed

--- src/environments/environments.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  // apiDomain: 'https://sunflowerofficialapi-production.up.railway.app/api',
-   apiDomain: 'http://localhost:3000/api',
-    googleClientId: '482435013042-0lbtsgfirgbtuaao8k86d7gvu4rg8sku.apps.googleusercontent.com',
-
-
-      truecallerAuthUrl: 'https://api4.truecaller.com/v1/oauth/authorize?client_id=your_truecaller_client_id&redirect_uri=http://localhost:4200/auth/truecaller/callback&response_type=code'
-};
