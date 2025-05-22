eb423ed3455fec262e63df2fac17b3ff9ecc27a9
SaqibShakil303 <mds168723@gmail.com>
2025-05-22 22:55:43 +0530

    added tour service

---
+++ src/app/services/tours/tour.service.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environments.dev';
+
+export interface Tour {
+  id: number;
+  title: string;
+  slug: string;
+  location: string;
+  description: string;
+  price: number;
+  image_url: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TourService {
+
+    constructor(private http: HttpClient,
+      @Inject(PLATFORM_ID) private platformId: Object
+    ) { }
+ 
+  private apiUrl = environment.apiDomain;
+
+
+  searchTours(query: string): Observable<Tour[]> {
+    return this.http.get<Tour[]>(`${this.apiUrl}/tours/search/q?q=${query}`);
+  }
+
+  getAllTours(): Observable<Tour[]> {
+    return this.http.get<Tour[]>(`${this.apiUrl}/tours`);
+  }
+}
