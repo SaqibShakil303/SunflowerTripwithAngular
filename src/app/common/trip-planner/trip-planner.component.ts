53e5774928768ea338f2ea958002a33e64dedd28
SaqibShakil303 <mds168723@gmail.com>
2025-06-13 12:30:42 +0530

    some changes pushing

---
+++ src/app/common/trip-planner/trip-planner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-planner',
+  standalone: true,
+  imports: [],
+  templateUrl: './trip-planner.component.html',
+  styleUrl: './trip-planner.component.scss'
+})
+export class TripPlannerComponent {
+
+}
