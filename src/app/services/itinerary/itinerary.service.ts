2cb78d7777f044175a5f63c99ae5dd187445adab
Amaanuddin Ahmed <ahmedamaanuddin@gmail.com>
2025-07-01 12:29:56 +0530

    standardized itinerary mgmt.

--- src/app/services/itinerary/itinerary.service.ts
+++ src/app/services/itinerary/itinerary.service.ts
@@ -13,35 +13,35 @@ export class ItineraryService {
   constructor(private http: HttpClient,
     @Inject(PLATFORM_ID) private platformId: Object
   ) { }
-private APIurl =environment.apiDomain
-    // submitItineraryDetail(f: Itinerary): Observable<any> {
-    //    const payload = {
-    //   name:   f.name,
-    //   email:  f.email,
-    //   phone:  f.phone,
-    //   destination: f.destination,
-    //   travelers: f.travelers,
-    //   children: f.children,
-    //   childAges: f.childAges,
-    //   duration: f.duration,
-    //   date: f.date,
-    //   budget: f.budget,
-    //   hotelCategory: f.hotelCategory,
-    //   travelType: f.travelType,
-    //   occupation: f.occupation,
-    //   preferences: f.preferences
-  
-    
-    // };
-    //     console.log('🪂 Sending payload to API:', payload);
-    // return this.http.post(`${this.APIurl}/Itinerary/AddItinerary`, f);
-    // //  return from(
-    // //   this.api 
-    // //     .post('/Itinerary/AddItinerary', payload)
-    // //     .then(r => r.data)
-    // // );
-    // }
-    submitItineraryDetail(itinerary: Itinerary): Observable<any> {
+  private APIurl = environment.apiDomain
+  // submitItineraryDetail(f: Itinerary): Observable<any> {
+  //    const payload = {
+  //   name:   f.name,
+  //   email:  f.email,
+  //   phone:  f.phone,
+  //   destination: f.destination,
+  //   travelers: f.travelers,
+  //   children: f.children,
+  //   childAges: f.childAges,
+  //   duration: f.duration,
+  //   date: f.date,
+  //   budget: f.budget,
+  //   hotelCategory: f.hotelCategory,
+  //   travelType: f.travelType,
+  //   occupation: f.occupation,
+  //   preferences: f.preferences
+
+
+  // };
+  //     console.log('🪂 Sending payload to API:', payload);
+  // return this.http.post(`${this.APIurl}/Itinerary/AddItinerary`, f);
+  // //  return from(
+  // //   this.api 
+  // //     .post('/Itinerary/AddItinerary', payload)
+  // //     .then(r => r.data)
+  // // );
+  // }
+  submitItineraryDetail(itinerary: Itinerary): Observable<any> {
     if (isPlatformBrowser(this.platformId)) {
       return this.http.post(`${this.APIurl}/Itinerary/AddItinerary`, itinerary);
     } else {
@@ -49,7 +49,11 @@ private APIurl =environment.apiDomain
       throw new Error('HTTP requests are not supported on the server');
     }
   }
-    getItineraries(): Observable<Itinerary[]> {
+  getItineraries(): Observable<Itinerary[]> {
     return this.http.get<Itinerary[]>(`${this.APIurl}/Itinerary/GetItineraries`);
+  }
+
+  deleteItinerary(id: number): Observable<any> {
+    return this.http.delete(`${this.APIurl}/Itinerary/DeleteItinerary/${id}`);
   }
 }
